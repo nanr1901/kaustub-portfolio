7df8ce924fa7e15c2e616f0dec40d46b6a39db6d
Kaustub Sreekrishnan <kaustubsreekrishnan@gmail.com>
2023-06-30 18:59:20 +0530

    Changed Intro image

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -8,8 +8,8 @@ function Intro() {
             <div className='row'>
             <div className='IntroText col-lg-6 col-md-12 col-sm-12'>
                 <h1>Hi I'm Kaustub Sreekrishnan</h1>
-                <h3>I'm a Computer Science Undergrad studying in BIT Mesra.</h3> 
-                <h3>I love learning and exploring new fields in the world of Computer Science</h3>
+                <h3 className='l1'>I'm a Computer Science Undergrad studying in BIT Mesra.</h3> 
+                <h3 className='l2'>I love learning and exploring new fields in the world of Computer Science</h3>
             </div>
             {/* <img src={img_url} alt='Cashew img' className='col-lg-6 col-md-12 col-sm-12'/> */}
             </div>
